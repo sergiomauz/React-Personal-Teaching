6e309cf9456c602bfe54e93cd3239165cdc82511
sergiomauz <sergio.mauz88@gmail.com>
2020-11-01 17:27:36 -0500

    Starting teacherlist

--- src/containers/TeachersList.js
+++ src/containers/TeachersList.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-const TeachersList = () => (
-  <h2>
-    TeachersList
-  </h2>
-);
+
+const TeachersList = () => {
+  return (
+    <>
+      <h2>
+        TeachersList
+      </h2>
+      <ul>
+        <li>Itemn 1</li>
+        <li>Itemn 2</li>
+        <li>Itemn 3</li>
+        <li>Itemn 4</li>
+        <li>Itemn 5</li>
+        <li>Itemn 6</li>
+      </ul>
+    </>
+  );
+};
 
 export default TeachersList;
